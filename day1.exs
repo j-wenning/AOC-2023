24ad3b8bb7a4fb15d23e1899327b49156403d213
j-wenning <wenning.justin@gmail.com>
2023-12-02 23:34:26 -0800

    Day 1 (Part 2) Solution

--- day1.exs
+++ day1.exs
@@ -1,28 +1,53 @@
 defmodule Calibration do
   def parse_input(input) do
-    String.split(input, "\n")
+    input
+    |> String.split("\n")
     |> Stream.filter(&(&1 != ""))
     |> Stream.map(&parse_line/1)
-    |> Stream.map(&parse_maybe_int/1)
     |> Enum.reduce(&Kernel.+/2)
   end
 
-  defp parse_maybe_int(maybe_int) do
-    case Integer.parse(maybe_int) do
-      {i, _} -> i
-      :error -> 0
+  @numeric_re ~r/\d|one|two|three|four|five|six|seven|eight|nine|zero/
+  defp parse_line(input) do
+    %{:first => first, :last => last} = branching_scan(%{}, @numeric_re, input)
+    parse_digit(first) * 10 + parse_digit(last)
+  end
+
+  defp parse_literal(int_str) do
+    {i, _} = Integer.parse(int_str)
+    i
+  end
+
+  defp parse_digit(str) do
+    case str do
+      "one" -> 1
+      "two" -> 2
+      "three" -> 3
+      "four" -> 4
+      "five" -> 5
+      "six" -> 6
+      "seven" -> 7
+      "eight" -> 8
+      "nine" -> 9
+      "zero" -> 0
+      _ -> parse_literal(str)
     end
   end
 
-  defp parse_line(input) do
-    %{:first => first, :last => last} =
-      String.graphemes(input)
-      |> Enum.reduce(%{}, fn x, acc ->
-        if Regex.match?(~r/\d/, x),
-          do: Map.put_new(acc, :first, x) |> Map.put(:last, x),
-          else: acc
-      end)
+  defp branching_scan(acc, _regex, "") do
+    acc
+  end
 
-    first <> last
+  defp branching_scan(acc, regex, target) do
+    result = Regex.run(regex, target, return: :index, capture: :first)
+
+    if result == nil do
+      acc
+    else
+      {idxStart, idxEnd} = List.first(result)
+      {_, tail} = String.split_at(target, idxStart + 1)
+      str = String.slice(target, idxStart, idxEnd)
+      acc |> Map.put_new(:first, str) |> Map.put(:last, str) |> branching_scan(regex, tail)
+    end
   end
 end
