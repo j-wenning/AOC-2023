88845b0712ba8efe7ac74cde7c0875f104753322
j-wenning <wenning.justin@gmail.com>
2023-12-03 17:07:27 -0800

    Redo day 1 part 2 with pattern matching

--- day1-part2.exs
+++ day1-part2.exs
@@ -1,53 +1,41 @@
 defmodule Calibration do
   def parse_input(input) do
     input
-    |> String.split("\n")
-    |> Stream.filter(&(&1 != ""))
+    |> String.split("\n", trim: true)
     |> Stream.map(&parse_line/1)
-    |> Enum.reduce(&Kernel.+/2)
+    |> Enum.sum()
   end
 
-  @numeric_re ~r/\d|one|two|three|four|five|six|seven|eight|nine|zero/
-  defp parse_line(input) do
-    %{:first => first, :last => last} = branching_scan(%{}, @numeric_re, input)
-    parse_digit(first) * 10 + parse_digit(last)
+  defp parse_line(line) do
+    len = String.length(line)
+
+    results =
+      0..len
+      |> Stream.map(fn idx -> String.slice(line, idx, len - idx) |> parse_digit() end)
+      |> Stream.reject(&is_nil/1)
+
+    Enum.at(results, 0) * 10 + Enum.at(results, -1)
   end
 
-  defp parse_literal(int_str) do
-    {i, _} = Integer.parse(int_str)
-    i
-  end
-
-  defp parse_digit(str) do
-    case str do
-      "one" -> 1
-      "two" -> 2
-      "three" -> 3
-      "four" -> 4
-      "five" -> 5
-      "six" -> 6
-      "seven" -> 7
-      "eight" -> 8
-      "nine" -> 9
-      "zero" -> 0
-      _ -> parse_literal(str)
-    end
-  end
-
-  defp branching_scan(acc, _regex, "") do
-    acc
-  end
-
-  defp branching_scan(acc, regex, target) do
-    result = Regex.run(regex, target, return: :index, capture: :first)
-
-    if result == nil do
-      acc
-    else
-      {idxStart, idxEnd} = List.first(result)
-      {_, tail} = String.split_at(target, idxStart + 1)
-      str = String.slice(target, idxStart, idxEnd)
-      acc |> Map.put_new(:first, str) |> Map.put(:last, str) |> branching_scan(regex, tail)
-    end
-  end
+  defp parse_digit("one" <> _), do: 1
+  defp parse_digit("two" <> _), do: 2
+  defp parse_digit("three" <> _), do: 3
+  defp parse_digit("four" <> _), do: 4
+  defp parse_digit("five" <> _), do: 5
+  defp parse_digit("six" <> _), do: 6
+  defp parse_digit("seven" <> _), do: 7
+  defp parse_digit("eight" <> _), do: 8
+  defp parse_digit("nine" <> _), do: 9
+  defp parse_digit("zero" <> _), do: 0
+  defp parse_digit("1" <> _), do: 1
+  defp parse_digit("2" <> _), do: 2
+  defp parse_digit("3" <> _), do: 3
+  defp parse_digit("4" <> _), do: 4
+  defp parse_digit("5" <> _), do: 5
+  defp parse_digit("6" <> _), do: 6
+  defp parse_digit("7" <> _), do: 7
+  defp parse_digit("8" <> _), do: 8
+  defp parse_digit("9" <> _), do: 9
+  defp parse_digit("0" <> _), do: 0
+  defp parse_digit(_), do: nil
 end
